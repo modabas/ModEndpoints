e13f43f24427b5962fac3ee72589cb82f19ff906
Melih Odabas <melih_odabas@hotmail.com>
2025-05-15 13:05:47 +0300

    - rollback changes to StreamingResponseItem

--- src/ModEndpoints.RemoteServices/StreamingResponseItem.cs
+++ src/ModEndpoints.RemoteServices/StreamingResponseItem.cs
@@ -5,47 +5,22 @@ namespace ModEndpoints.RemoteServices;
 /// <summary>
 /// Represents a streaming service endpoint response item.
 /// </summary>
-public record StreamingResponseItem
-{
-  internal readonly string? _responseType;
-  public Result Result { get; init; }
-  public string ResponseType => _responseType ?? StreamingResponseItemDefinitions.DefaultResponseType;
-  public string? ResponseId { get; init; }
-
-  public StreamingResponseItem(
-    Result Result,
-    string? ResponseType = null,
-    string? ResponseId = null)
-  {
-    this.Result = Result;
-    _responseType = ResponseType;
-    this.ResponseId = ResponseId;
-  }
-}
+public record StreamingResponseItem(
+  Result Result,
+  string? ResponseType = StreamingResponseItemDefinitions.DefaultResponseType,
+  string? ResponseId = null);
 
 /// <summary>
 /// Represents a streaming service endpoint response item.
 /// </summary>
 /// <typeparam name="TResponse">Specifies the type of data payload in the response.</typeparam>
-public record StreamingResponseItem<TResponse>
+public record StreamingResponseItem<TResponse>(
+  Result<TResponse> Result,
+  string? ResponseType = StreamingResponseItemDefinitions.DefaultResponseType,
+  string? ResponseId = null)
   where TResponse : notnull
 {
-  internal readonly string? _responseType;
-  public Result<TResponse> Result { get; init; }
-  public string ResponseType => _responseType ?? StreamingResponseItemDefinitions.DefaultResponseType;
-  public string? ResponseId { get; init; }
-
-  public StreamingResponseItem(
-    Result<TResponse> Result,
-    string? ResponseType = null,
-    string? ResponseId = null)
-  {
-    this.Result = Result;
-    _responseType = ResponseType;
-    this.ResponseId = ResponseId;
-  }
-
-  public StreamingResponseItem ToItem() 
+  public StreamingResponseItem ToItem()
     => new(
       Result: Result,
       ResponseType: ResponseType,
