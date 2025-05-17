9599f052f1085ea2768347e3e3e04db6c38b6f57
Melih Odabas <melih_odabas@hotmail.com>
2025-05-17 17:02:20 +0300

    - Removed try block to catch and wrap exception in a Result object in DefaultServiceChannel SendAsync methods. Leave it to caller to handle exceptions.

--- src/ModEndpoints.RemoteServices/DefaultServiceChannel.cs
+++ src/ModEndpoints.RemoteServices/DefaultServiceChannel.cs
@@ -22,48 +22,41 @@ public class DefaultServiceChannel(
     string? serializerName = null)
     where TResponse : notnull
   {
-    try
+    using (var scope = serviceProvider.CreateScope())
     {
-      using (var scope = serviceProvider.CreateScope())
+      var uriResolver = scope.ServiceProvider.GetRequiredKeyedService<IServiceEndpointUriResolver>(
+        uriResolverName ?? RemoteServiceDefinitions.DefaultUriResolverName);
+      var serializer = scope.ServiceProvider.GetRequiredKeyedService<IServiceChannelSerializer>(
+        serializerName ?? RemoteServiceDefinitions.DefaultSerializerName);
+      var requestUriResult = uriResolver.Resolve(req);
+      if (requestUriResult.IsFailed)
       {
-        var uriResolver = scope.ServiceProvider.GetRequiredKeyedService<IServiceEndpointUriResolver>(
-          uriResolverName ?? RemoteServiceDefinitions.DefaultUriResolverName);
-        var serializer = scope.ServiceProvider.GetRequiredKeyedService<IServiceChannelSerializer>(
-          serializerName ?? RemoteServiceDefinitions.DefaultSerializerName);
-        var requestUriResult = uriResolver.Resolve(req);
-        if (requestUriResult.IsFailed)
+        return Result<TResponse>.Fail(requestUriResult);
+      }
+      var requestType = req.GetType();
+      if (!ServiceChannelRegistry.Instance.IsRequestRegistered(requestType, out var clientName))
+      {
+        return Result<TResponse>.CriticalError(string.Format(NoChannelRegistrationFound, requestType));
+      }
+      using (HttpRequestMessage httpReq = new(
+        HttpMethod.Post,
+        Combine(endpointUriPrefix, requestUriResult.Value)))
+      {
+        httpReq.Content = await serializer.CreateContentAsync(req, ct);
+        if (httpRequestInterceptor is not null)
         {
-          return Result<TResponse>.Fail(requestUriResult);
+          await httpRequestInterceptor(scope.ServiceProvider, httpReq, ct);
         }
-        var requestType = req.GetType();
-        if (!ServiceChannelRegistry.Instance.IsRequestRegistered(requestType, out var clientName))
+        var client = clientFactory.CreateClient(clientName);
+        using (var httpResponse = await client.SendAsync(httpReq, ct))
         {
-          return Result<TResponse>.CriticalError(string.Format(NoChannelRegistrationFound, requestType));
-        }
-        using (HttpRequestMessage httpReq = new(
-          HttpMethod.Post,
-          Combine(endpointUriPrefix, requestUriResult.Value)))
-        {
-          httpReq.Content = await serializer.CreateContentAsync(req, ct);
-          if (httpRequestInterceptor is not null)
+          if (httpResponseInterceptor is not null)
           {
-            await httpRequestInterceptor(scope.ServiceProvider, httpReq, ct);
+            await httpResponseInterceptor(scope.ServiceProvider, httpResponse, ct);
           }
-          var client = clientFactory.CreateClient(clientName);
-          using (var httpResponse = await client.SendAsync(httpReq, ct))
-          {
-            if (httpResponseInterceptor is not null)
-            {
-              await httpResponseInterceptor(scope.ServiceProvider, httpResponse, ct);
-            }
-            return await serializer.DeserializeResultAsync<TResponse>(httpResponse, ct);
-          }
+          return await serializer.DeserializeResultAsync<TResponse>(httpResponse, ct);
         }
       }
-    }
-    catch (Exception ex)
-    {
-      return ex;
     }
   }
 
@@ -76,48 +69,41 @@ public class DefaultServiceChannel(
     string? uriResolverName = null,
     string? serializerName = null)
   {
-    try
+    using (var scope = serviceProvider.CreateScope())
     {
-      using (var scope = serviceProvider.CreateScope())
+      var uriResolver = scope.ServiceProvider.GetRequiredKeyedService<IServiceEndpointUriResolver>(
+        uriResolverName ?? RemoteServiceDefinitions.DefaultUriResolverName);
+      var serializer = scope.ServiceProvider.GetRequiredKeyedService<IServiceChannelSerializer>(
+        serializerName ?? RemoteServiceDefinitions.DefaultSerializerName);
+      var requestUriResult = uriResolver.Resolve(req);
+      if (requestUriResult.IsFailed)
       {
-        var uriResolver = scope.ServiceProvider.GetRequiredKeyedService<IServiceEndpointUriResolver>(
-          uriResolverName ?? RemoteServiceDefinitions.DefaultUriResolverName);
-        var serializer = scope.ServiceProvider.GetRequiredKeyedService<IServiceChannelSerializer>(
-          serializerName ?? RemoteServiceDefinitions.DefaultSerializerName);
-        var requestUriResult = uriResolver.Resolve(req);
-        if (requestUriResult.IsFailed)
+        return Result.Fail(requestUriResult);
+      }
+      var requestType = req.GetType();
+      if (!ServiceChannelRegistry.Instance.IsRequestRegistered(requestType, out var clientName))
+      {
+        return Result.CriticalError(string.Format(NoChannelRegistrationFound, requestType));
+      }
+      using (HttpRequestMessage httpReq = new(
+        HttpMethod.Post,
+        Combine(endpointUriPrefix, requestUriResult.Value)))
+      {
+        httpReq.Content = await serializer.CreateContentAsync(req, ct);
+        if (httpRequestInterceptor is not null)
         {
-          return Result.Fail(requestUriResult);
+          await httpRequestInterceptor(scope.ServiceProvider, httpReq, ct);
         }
-        var requestType = req.GetType();
-        if (!ServiceChannelRegistry.Instance.IsRequestRegistered(requestType, out var clientName))
+        var client = clientFactory.CreateClient(clientName);
+        using (var httpResponse = await client.SendAsync(httpReq, ct))
         {
-          return Result.CriticalError(string.Format(NoChannelRegistrationFound, requestType));
-        }
-        using (HttpRequestMessage httpReq = new(
-          HttpMethod.Post,
-          Combine(endpointUriPrefix, requestUriResult.Value)))
-        {
-          httpReq.Content = await serializer.CreateContentAsync(req, ct);
-          if (httpRequestInterceptor is not null)
+          if (httpResponseInterceptor is not null)
           {
-            await httpRequestInterceptor(scope.ServiceProvider, httpReq, ct);
+            await httpResponseInterceptor(scope.ServiceProvider, httpResponse, ct);
           }
-          var client = clientFactory.CreateClient(clientName);
-          using (var httpResponse = await client.SendAsync(httpReq, ct))
-          {
-            if (httpResponseInterceptor is not null)
-            {
-              await httpResponseInterceptor(scope.ServiceProvider, httpResponse, ct);
-            }
-            return await serializer.DeserializeResultAsync(httpResponse, ct);
-          }
+          return await serializer.DeserializeResultAsync(httpResponse, ct);
         }
       }
-    }
-    catch (Exception ex)
-    {
-      return ex;
     }
   }
 
