876affc8a187877be99cb7f4d28d2429f6c30f53
Melih Odabas <melih_odabas@hotmail.com>
2025-05-21 09:00:46 +0300

    - code cleanup

--- src/ModEndpoints.Core/Constants.cs
+++ src/ModEndpoints.Core/Constants.cs
@@ -1,11 +1,11 @@
 ﻿namespace ModEndpoints.Core;
 internal static class Constants
 {
-  public const string RouteBuilderIsNullForEndpointMessage = 
+  public const string RouteBuilderIsNullForEndpointMessage =
     "Route builder is null for {0} endpoint.";
-  public const string RequiredServiceIsInvalidMessage = 
+  public const string RequiredServiceIsInvalidMessage =
     "Service resolved from dependency injection is invalid.";
-  public const string MissingRouteGroupConfigurationMessage = 
+  public const string MissingRouteGroupConfigurationMessage =
     "Missing route group configuration! Start configuring {0} route group by calling the MapGroup method of the builder.";
   public const string MissingRouteGroupConfigurationLogMessage =
     "Missing route group configuration! Start configuring {routeGroupType} route group by calling the MapGroup method of the builder.";
