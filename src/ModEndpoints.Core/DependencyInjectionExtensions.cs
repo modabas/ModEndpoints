85f4f38daef53d6b3d3a92d4bad50186758b7bb9
Melih Odabas <melih_odabas@hotmail.com>
2025-05-21 09:53:50 +0300

    - add summary to di methods.

--- src/ModEndpoints.Core/DependencyInjectionExtensions.cs
+++ src/ModEndpoints.Core/DependencyInjectionExtensions.cs
@@ -9,6 +9,16 @@ using ModEndpoints.RemoteServices.Core;
 namespace ModEndpoints.Core;
 public static class DependencyInjectionExtensions
 {
+  /// <summary>
+  /// Registers route groups and endpoints from the assembly containing the specified type into the dependency injection container. 
+  /// </summary>
+  /// <remarks>This method scans the assembly containing the specified type for components and registers them with the
+  /// dependency injection container.</remarks>
+  /// <typeparam name="T"></typeparam>
+  /// <param name="services">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+  /// <param name="configure">An optional delegate to configure <see cref="ModEndpointsCoreOptions"/> for customizing the behavior of the
+  /// registration process.</param>
+  /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
   public static IServiceCollection AddModEndpointsCoreFromAssemblyContaining<T>(
     this IServiceCollection services,
     Action<ModEndpointsCoreOptions>? configure = null)
@@ -16,6 +26,16 @@ public static class DependencyInjectionExtensions
     return services.AddModEndpointsCoreFromAssembly(typeof(T).Assembly, configure);
   }
 
+  /// <summary>
+  /// Registers route groups and endpoints from the specified assembly into the dependency injection container.
+  /// </summary>
+  /// <remarks>This method scans the specified assembly for components and registers them with the
+  /// dependency injection container.</remarks>
+  /// <param name="services">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+  /// <param name="assembly">The assembly containing components to be registered.</param>
+  /// <param name="configure">An optional delegate to configure <see cref="ModEndpointsCoreOptions"/> for customizing the behavior of the
+  /// registration process.</param>
+  /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
   public static IServiceCollection AddModEndpointsCoreFromAssembly(
     this IServiceCollection services,
     Assembly assembly,
@@ -137,10 +157,16 @@ public static class DependencyInjectionExtensions
     return true;
   }
 
-  //Configuration processing order:
-  //Group's Configuration -> Endpoint's Configuration -> Global Endpoint Configuration -> Endpoint's PostConfigure -> Group's EndpointPostConfigure -> Group's PostConfigure
-  //Global Endpoint Configuration apply to all endpoints
-  //Group's EndpointPostConfigure apply to endpoints directly under that group (not if they are under a child group of current group)
+  /// <summary>
+  /// Maps and configures route groups and endpoints. Configuration processing order:
+  /// Group's Configuration -> Endpoint's Configuration -> Global Endpoint Configuration -> Endpoint's PostConfigure -> Group's EndpointPostConfigure -> Group's PostConfigure.
+  /// Global Endpoint Configuration apply to all endpoints.
+  /// Group's EndpointPostConfigure apply to endpoints directly under that group (not if they are under a child group of current group).
+  /// </summary>
+  /// <param name="app"></param>
+  /// <param name="globalEndpointConfiguration">Endpoint configuration to be applied to all endpoints.</param>
+  /// <param name="throwOnMissingConfiguration"></param>
+  /// <returns></returns>
   public static WebApplication MapModEndpointsCore(
     this WebApplication app,
     Action<RouteHandlerBuilder, ConfigurationContext<EndpointConfigurationParameters>>? globalEndpointConfiguration = null,
--- src/ModEndpoints/DependencyInjectionExtensions.cs
+++ src/ModEndpoints/DependencyInjectionExtensions.cs
@@ -8,6 +8,16 @@ using ModEndpoints.RemoteServices;
 namespace ModEndpoints;
 public static class DependencyInjectionExtensions
 {
+  /// <summary>
+  /// Registers route groups and endpoints from the assembly containing the specified type into the dependency injection container. 
+  /// </summary>
+  /// <remarks>This method scans the assembly containing the specified type for components and registers them with the
+  /// dependency injection container.</remarks>
+  /// <typeparam name="T"></typeparam>
+  /// <param name="services">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+  /// <param name="configure">An optional delegate to configure <see cref="ModEndpointsCoreOptions"/> for customizing the behavior of the
+  /// registration process.</param>
+  /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
   public static IServiceCollection AddModEndpointsFromAssemblyContaining<T>(
     this IServiceCollection services,
     Action<ModEndpointsOptions>? configure = null)
@@ -15,6 +25,16 @@ public static class DependencyInjectionExtensions
     return services.AddModEndpointsFromAssembly(typeof(T).Assembly, configure);
   }
 
+  /// <summary>
+  /// Registers route groups and endpoints from the specified assembly into the dependency injection container.
+  /// </summary>
+  /// <remarks>This method scans the specified assembly for components and registers them with the
+  /// dependency injection container.</remarks>
+  /// <param name="services">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+  /// <param name="assembly">The assembly containing components to be registered.</param>
+  /// <param name="configure">An optional delegate to configure <see cref="ModEndpointsCoreOptions"/> for customizing the behavior of the
+  /// registration process.</param>
+  /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
   public static IServiceCollection AddModEndpointsFromAssembly(
     this IServiceCollection services,
     Assembly assembly,
@@ -49,6 +69,16 @@ public static class DependencyInjectionExtensions
       });
   }
 
+  /// <summary>
+  /// Maps and configures route groups and endpoints. Configuration processing order:
+  /// Group's Configuration -> Endpoint's Configuration -> Global Endpoint Configuration -> Endpoint's PostConfigure -> Group's EndpointPostConfigure -> Group's PostConfigure.
+  /// Global Endpoint Configuration apply to all endpoints.
+  /// Group's EndpointPostConfigure apply to endpoints directly under that group (not if they are under a child group of current group).
+  /// </summary>
+  /// <param name="app"></param>
+  /// <param name="globalEndpointConfiguration">Endpoint configuration to be applied to all endpoints.</param>
+  /// <param name="throwOnMissingConfiguration"></param>
+  /// <returns></returns>
   public static WebApplication MapModEndpoints(
     this WebApplication app,
     Action<RouteHandlerBuilder, ConfigurationContext<EndpointConfigurationParameters>>? globalEndpointConfiguration = null,
