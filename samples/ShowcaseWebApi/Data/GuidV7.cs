688262f34fc441365096a2bd31855bf1223f86e8
Melih Odabas <melih_odabas@hotmail.com>
2025-11-18 16:19:27 +0300

    - move ShowcaseWebApi sample project from .net 8.0 to .net 10

--- samples/ShowcaseWebApi/Data/GuidV7.cs
+++ samples/ShowcaseWebApi/Data/GuidV7.cs
@@ -1,11 +1,9 @@
-﻿using UUIDNext;
-
-namespace ShowcaseWebApi.Data;
+﻿namespace ShowcaseWebApi.Data;
 
 public static class GuidV7
 {
   public static Guid CreateVersion7()
   {
-    return Uuid.NewDatabaseFriendly(Database.PostgreSql);
+    return Guid.CreateVersion7();
   }
 }
